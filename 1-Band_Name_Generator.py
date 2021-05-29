eb2e88292b9ad0d8f24a484e3b4ff83f413975e1
TimothyJan <jan.timothy@gmail.com>
2021-05-29 15:00:12 -0700

    Update to Day 5

--- 1-Band_Name_Generator.py
+++ 1-Band_Name_Generator.py
@@ -1,3 +1,5 @@
+# Day 1 - Band Name Generator
+# Working with Variables in Python to Manage Data
 
 print("Welcome to the Band Name Generator.")
 street = input("What's name of the city you grew up in?\n")
