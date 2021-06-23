9b886c66c3861d58bd794fe621fe197cf4360336
TimothyJan <jan.timothy@gmail.com>
2021-06-22 19:15:12 -0700

    Send Email when ISS in the sky and is night

--- 33-API_Interface_Parameters/ISS_Overhead_Notifier/main.py
+++ 33-API_Interface_Parameters/ISS_Overhead_Notifier/main.py
@@ -1,8 +1,12 @@
 import requests
-from datetime import datetime, time
+from datetime import datetime, timezone
+import smtplib
+import time
 
 MY_LAT = 34.133500
 MY_LONG = -118.027020
+my_email = "afe4ther@gmail.com"
+password = "fbjhaxc5"
 
 def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -29,7 +33,7 @@ def is_night():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-    time_now = datetime.now().hour
+    time_now = datetime.now(timezone.utc).hour
     if time_now >= sunset or time_now <= sunrise:
         return True
 
@@ -37,6 +41,14 @@ def is_night():
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
-if is_iss_overhead() and is_night():
-    
+while True:
+    time.sleep(60) # execute every 60 seconds
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg="Subject: Loop up into the sky!\n\nThe ISS is above you in the sky!")
 
